d237dbaa7f89f1456ccd5f5ca082e12175bcd832
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-18 11:55:57 +0100

    Commit netflix002 y netflix003

---
+++ netflix-003/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/compras", (req, res) => {
+  res.send("Bienvenido a la app de compras");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
+
